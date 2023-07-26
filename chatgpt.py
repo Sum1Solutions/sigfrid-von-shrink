2f750af5d468514e77cee25f32d54d303924cd18
Sum1Solutions <17915475+Sum1Solutions@users.noreply.github.com>
2023-07-26 09:57:36 -0400

    Moving key configuration to .env

--- chatgpt.py
+++ chatgpt.py
@@ -1,5 +1,9 @@
 import openai
 import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 def chat_with_gpt(message):
     try:
@@ -7,16 +11,17 @@ def chat_with_gpt(message):
         api_key = os.environ.get('CHATGPT_API_KEY')
         openai.api_key = api_key  # Set OpenAI API key
 
-        # Define the system prompt and user prompt
-        system_prompt = "You are an AI therapist that behaves like Sigfrid von Shrink.\n\nUser: "
-        user_prompt = f"{system_prompt}{message}\n"
+        # Get engine and prompt from environment variables
+        engine = os.environ.get('ENGINE')
+        system_prompt = os.environ.get('SYSTEM_PROMPT')
+        user_prompt = f"{system_prompt}\n\nUser: {message}\n"
 
         # Generate the chat response using OpenAI's Python package
         response = openai.Completion.create(
-            engine='text-davinci-003',
+            engine=engine,
             prompt=user_prompt,
             temperature=0.7,
-            max_tokens=100,
+            max_tokens=1000,
             n=1,
             stop=None
         )

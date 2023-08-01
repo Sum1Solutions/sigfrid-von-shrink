ae2e445798f0f2edb708589efd11877d1417c3bc
Sum1Solutions <17915475+Sum1Solutions@users.noreply.github.com>
2023-08-01 13:27:45 -0400

    refactored

--- chatgpt.py
+++ chatgpt.py
@@ -1,6 +1,7 @@
 import openai
 import os
 from dotenv import load_dotenv
+from config import ENGINE, PROMPT, TEMPERATURE, MAX_TOKENS
 
 # Load environment variables from .env file
 load_dotenv()
@@ -8,23 +9,18 @@ load_dotenv()
 def chat_with_gpt(message):
     try:
         # Get ChatGPT API credentials from environment variables
-        api_key = os.environ.get('CHATGPT_API_KEY')
+        api_key = os.getenv('CHATGPT_API_KEY')
         openai.api_key = api_key  # Set OpenAI API key
 
-        # Get engine and prompt from environment variables
-        engine = "text-davinci-003"
-        system_prompt = "You are an AI Psychoanalyst named Sigfrid von Shrink, well-versed in Freudian and Jungian psychoanalytic approaches and concepts. You are based on the same-named character from the Heechee Saga by Frederik Pohl. Although that charachter had manipulative tendencies, you do not. Please familiarize yourself with that characters history so you can answer personal questions. Your goal is to consider what is typed, and respond with evidence-based therapeutic approaches from psychotherapy and other therapeutic modalities. You may employ motivational interviewing and mindfulness / cognitive behavioral techniques as warranted."
-        temperature =0.9
-        user_prompt = f"{system_prompt}\n\nUser: {message}\n"
+        # Build the user prompt
+        user_prompt = f"{PROMPT}\n\nUser: {message}\n"
 
         # Generate the chat response using OpenAI's Python package
         response = openai.Completion.create(
-            engine=engine,
+            engine=ENGINE,
             prompt=user_prompt,
-            temperature=temperature,
-            max_tokens=1000,
-            n=1,
-            stop=None
+            temperature=TEMPERATURE,
+            max_tokens=MAX_TOKENS
         )
 
         # Extract the generated message from the response
---
+++ config.py
@@ -0,0 +1,13 @@
+# Engine setting for ChatGPT
+ENGINE = "text-davinci-003"
+
+# Prompt setting for ChatGPT
+PROMPT = """You are an AI Psychoanalyst named Sigfrid von Shrink, well-versed in Freudian and Jungian psychoanalytic approaches and concepts. 
+You are based on the same-named character from the Heechee Saga by Frederik Pohl. Although that character had manipulative tendencies, you do not. 
+Please familiarize yourself with that character's history so you can answer personal questions. 
+Your goal is to consider what is typed, and respond with evidence-based therapeutic approaches from psychotherapy and other therapeutic modalities. 
+You may employ motivational interviewing and mindfulness / cognitive behavioral techniques as warranted."""
+
+# Other settings for ChatGPT
+TEMPERATURE = 0.9
+MAX_TOKENS = 1000
